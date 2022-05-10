c413f693e46a4262bf53cf2f083e3103c96feb53
Francisco Mella <francisco@DESKTOP-07J4MKR.localdomain>
2022-05-10 15:23:38 +1200

    Master Repository

---
+++ boards/IMU/board_config.h
@@ -0,0 +1,15 @@
+#ifndef BOARD_CONFIG_H
+#define BOARD_CONFIG_H
+
+#define VENDOR_NAME "CawthronInstitute"
+#define PRODUCT_NAME "OpenOceanKinetics"
+#define VOLUME_LABEL "Cawthron"
+#define INDEX_URL "http://www.cawthron.org.nz/"
+#define BOARD_ID "Cawthron-id1-FrMella_v1.1a"
+
+#define USB_VID 0x239A
+#define USB_PID 0x0015
+
+#define LED_PIN PIN_PA17
+
+#endif
---
+++ build/CawthronKinetics/uf2_version.h
@@ -0,0 +1,1 @@
+#define UF2_VERSION_BASE "v3.4.0-70-gb393cb1-dirty"
